73004e8a91a51a63ffc08ecf0abc22d1801858bc
misiteye <32428777+misiteye@users.noreply.github.com>
2022-10-20 14:27:30 +0800

    123

--- db300.py
+++ db300.py
@@ -5,13 +5,11 @@ from clickhouse_driver import Client
 data={}
 with open("./data.json","r") as f:
     a=f.readlines()
-    for i in range(0,1):
+    for i in range(0,len(a)):
         i1=json.loads(a[i])
 #insert into stock.stock(message) values
-        sql_text='truncate table stock.stock;'
         sql_text='insert into stock.stock(message) values (\'' + i1 + '\');'
         print(sql_text)
-        client = Client('10.100.239.2')
 
 
 
